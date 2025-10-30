ac809078acf92ef7cf30331ff50ff1c47c0ef9bc
omkar506 <omkar.kotamire@pelsoftlabs.in>
2025-10-30 12:53:30 +0530

    Initial commit: add CounterAPI and counter-client

---
+++ CounterAPI/Controllers/CounterController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CounterAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CounterController : ControllerBase
+    {
+        private static int GlobalCounter = 0;
+
+        [HttpGet("global")]
+        public IActionResult GetGlobalCounter() => Ok(GlobalCounter);
+
+        [HttpPost("global/increment")]
+        public IActionResult IncrementGlobalCounter()
+        {
+            GlobalCounter++;
+            return Ok(GlobalCounter);
+        }
+    }
+}
